0eeea2387d37dd78c78f47494411f0c83baa3922
kammy92 <karman.singhh@gmail.com>
2017-09-03 17:14:02 +0530

    Completed with home screen

--- app/src/main/java/com/karman/ledgerex/activity/MainActivity.java
+++ app/src/main/java/com/karman/ledgerex/activity/MainActivity.java
@@ -2,8 +2,12 @@ package com.karman.ledgerex.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.karman.ledgerex.R;
 import com.karman.ledgerex.utils.Utils;
@@ -11,6 +15,10 @@ import com.karman.ledgerex.utils.Utils;
 public class MainActivity extends AppCompatActivity {
     public static boolean login = false;
     ImageView ivNavigation;
+    View bottomSheet;
+    ImageView ivSwipe;
+    RelativeLayout rlBottomSheet;
+    private BottomSheetBehavior mBottomSheetBehavior;
     
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -18,17 +26,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
         initView ();
         initData ();
+        initListener ();
         isLogin ();
     }
     
+    
     private void initView () {
+        bottomSheet = findViewById (R.id.bottom_sheet);
         ivNavigation = (ImageView) findViewById (R.id.ivNavigation);
+        ivSwipe = (ImageView) findViewById (R.id.ivSwipe);
+        rlBottomSheet = (RelativeLayout) findViewById (R.id.rlBottomSheet);
     }
     
     private void initData () {
+        mBottomSheetBehavior = BottomSheetBehavior.from (bottomSheet);
+        mBottomSheetBehavior.setPeekHeight ((int) Utils.pxFromDp (this, 76));
+        mBottomSheetBehavior.setState (BottomSheetBehavior.STATE_COLLAPSED);
         Utils.setTypefaceToAllViews (this, ivNavigation);
     }
     
+    private void initListener () {
+        rlBottomSheet.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                if (mBottomSheetBehavior.getState () == BottomSheetBehavior.STATE_EXPANDED) {
+                    mBottomSheetBehavior.setState (BottomSheetBehavior.STATE_COLLAPSED);
+                } else {
+                    mBottomSheetBehavior.setState (BottomSheetBehavior.STATE_EXPANDED);
+                }
+                
+            }
+        });
+        mBottomSheetBehavior.setBottomSheetCallback (new BottomSheetBehavior.BottomSheetCallback () {
+            @Override
+            public void onStateChanged (@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        ivSwipe.setImageResource (R.drawable.ic_swipe_down);
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        ivSwipe.setImageResource (R.drawable.ic_swipe_up);
+                        break;
+                }
+            }
+            
+            @Override
+            public void onSlide (@NonNull View bottomSheet, float slideOffset) {
+                // React to dragging events
+            }
+        });
+    }
+   
     private void isLogin () {
         if (! login) {
             Intent intent = new Intent (MainActivity.this, LoginActivity.class);
---
+++ app/src/main/java/com/karman/ledgerex/activity/SendActivity.java
@@ -0,0 +1,140 @@
+package com.karman.ledgerex.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.karman.ledgerex.R;
+import com.karman.ledgerex.utils.Utils;
+
+
+public class SendActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tvNext;
+    RelativeLayout rlBack;
+    CoordinatorLayout clMain;
+    
+    TextView tvPin1, tvPin2, tvPin3, tvPin4;
+    Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt0, btPeriod;
+    ImageView ivDelete, ivNext;
+    EditText etPIN;
+    
+    LinearLayout llPinButtons;
+    
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_login);
+        initView ();
+        initData ();
+        initListener ();
+    }
+    
+    private void initData () {
+        Utils.setTypefaceToAllViews (this, rlBack);
+    }
+    
+    private void initView () {
+        clMain = (CoordinatorLayout) findViewById (R.id.clMain);
+        bt1 = (Button) findViewById (R.id.bt1);
+        bt2 = (Button) findViewById (R.id.bt2);
+        bt3 = (Button) findViewById (R.id.bt3);
+        bt4 = (Button) findViewById (R.id.bt4);
+        bt5 = (Button) findViewById (R.id.bt5);
+        bt6 = (Button) findViewById (R.id.bt6);
+        bt7 = (Button) findViewById (R.id.bt7);
+        bt8 = (Button) findViewById (R.id.bt8);
+        bt9 = (Button) findViewById (R.id.bt9);
+        bt0 = (Button) findViewById (R.id.bt0);
+        btPeriod = (Button) findViewById (R.id.btPeriod);
+        rlBack = (RelativeLayout) findViewById (R.id.rlBack);
+        ivDelete = (ImageView) findViewById (R.id.ivDelete);
+        ivNext = (ImageView) findViewById (R.id.ivNext);
+        etPIN = (EditText) findViewById (R.id.etPIN);
+        tvPin1 = (TextView) findViewById (R.id.tvPin1);
+        tvPin2 = (TextView) findViewById (R.id.tvPin2);
+        tvPin3 = (TextView) findViewById (R.id.tvPin3);
+        tvPin4 = (TextView) findViewById (R.id.tvPin4);
+        llPinButtons = (LinearLayout) findViewById (R.id.llPinButton);
+    }
+    
+    private void initListener () {
+        
+        ivDelete.setOnClickListener (this);
+        bt0.setOnClickListener (this);
+        bt1.setOnClickListener (this);
+        bt2.setOnClickListener (this);
+        bt3.setOnClickListener (this);
+        bt4.setOnClickListener (this);
+        bt5.setOnClickListener (this);
+        bt6.setOnClickListener (this);
+        bt7.setOnClickListener (this);
+        bt8.setOnClickListener (this);
+        bt9.setOnClickListener (this);
+    }
+    
+    private boolean checkPIN (int pin) {
+        // Check the PIN entered. We use 1234 as default PIN
+        return pin == 1234;
+    }
+    
+    @Override
+    public void onClick (View v) {
+        String pin;
+        switch (v.getId ()) {
+            case R.id.bt0:
+                pin = etPIN.getText ().toString () + "0";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt1:
+                pin = etPIN.getText ().toString () + "1";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt2:
+                pin = etPIN.getText ().toString () + "2";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt3:
+                pin = etPIN.getText ().toString () + "3";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt4:
+                pin = etPIN.getText ().toString () + "4";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt5:
+                pin = etPIN.getText ().toString () + "5";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt6:
+                pin = etPIN.getText ().toString () + "6";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt7:
+                pin = etPIN.getText ().toString () + "7";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt8:
+                pin = etPIN.getText ().toString () + "8";
+                etPIN.setText (pin);
+                break;
+            case R.id.bt9:
+                pin = etPIN.getText ().toString () + "9";
+                etPIN.setText (pin);
+                break;
+            case R.id.ivDelete:
+                if (etPIN.getText ().toString ().length () > 0) {
+                    etPIN.setText (etPIN.getText ().toString ().substring (0, etPIN.getText ().toString ().length () - 1));
+                }
+                break;
+            
+        }
+        
+    }
+}
